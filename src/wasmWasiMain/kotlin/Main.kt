fb65064b62f4304cd3b9a45a743aaa030d76a2f7
riad <riadzaid100@gmail.com>
2026-04-18 14:45:34 +0200

    readln not supported in wasm on kotlin according to the source code

--- src/wasmWasiMain/kotlin/Main.kt
+++ src/wasmWasiMain/kotlin/Main.kt
@@ -6,3 +6,6 @@ fun main() {
 
 // https://github.com/JetBrains/kotlin/blob/2.3.20/libraries/stdlib/wasm/wasi/src/kotlin/io.kt#L98
 // readLn is not supported it is TODO.....
+
+// https://github.com/JetBrains/kotlin/blob/2.3.20/libraries/stdlib/wasm/js/src/kotlin/io.kt
+// also not in wasm js
