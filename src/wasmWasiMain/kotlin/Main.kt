6b17c840060002235f8f8e12b1b06f2e2f0ed60b
riad <riadzaid100@gmail.com>
2026-04-18 14:43:28 +0200

    readln not supported in wasm on kotlin according to the source code

--- src/wasmWasiMain/kotlin/Main.kt
+++ src/wasmWasiMain/kotlin/Main.kt
@@ -1,6 +1,8 @@
-
 fun main() {
-    println("test")
-    val input = readln()
-    println("Wasm received: $input")
+    println("Hello from Kotlin/Wasm!")
+    println("Note: stdin is not fully supported in Node.js WASI")
+    println("Use command line args or read from files instead")
 }
+
+// https://github.com/JetBrains/kotlin/blob/2.3.20/libraries/stdlib/wasm/wasi/src/kotlin/io.kt#L98
+// readLn is not supported it is TODO.....
