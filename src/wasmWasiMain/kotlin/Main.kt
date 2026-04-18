2ea44bb5c054479f2495ee01208816557cba1691
riad <riadzaid100@gmail.com>
2026-04-18 12:29:14 +0200

    crashes on readln input. has to do with nodejs runtime. compilation is succesfull.

--- src/wasmWasiMain/kotlin/Main.kt
+++ src/wasmWasiMain/kotlin/Main.kt
@@ -1,8 +1,6 @@
 
 fun main() {
-    println("--- WASI Echo Program ---")
-    println("Type something and press Enter (type 'exit' to stop):")
-
-
-    println("Goodbye!")
+    println("test")
+    val input = readln()
+    println("Wasm received: $input")
 }
